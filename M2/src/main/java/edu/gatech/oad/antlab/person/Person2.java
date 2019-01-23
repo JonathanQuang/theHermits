18af90b6e5ca42b3ccaad3c810fef361660d94c9
Gabriel Wang <43443158+gabriel-q-wang@users.noreply.github.com>
2019-01-23 08:11:38 -0500

    Changed Ant file, removed useless text

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string
  *
- * @author Bob
- * @version 1.1
+ * @author Gabriel Wang
+ * @version 1.2
  */
 public class Person2 {
     /** Holds the persons real name */
