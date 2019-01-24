b8314e66894e4e57c2e361571a79783df62dc324
Varnika <43051024+Vivacia@users.noreply.github.com>
2019-01-24 18:14:45 -0500

    Add files via upload
    adding changed files to my branch

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,10 +3,10 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string
- *
- *  @author Gabriel Wang
- *  @version 1.2
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
  */
 public class Person1 {
   /** Holds the persons real name */
@@ -30,17 +30,16 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-        String result = input.substring(2,input.length());
-        result += input.substring(0,2);
-        return result;
+	  //Person 1 put your implementation here
+	  return null;
 	}
-
+	
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the
+	 * @return the string representing the 
 	 *         object
 	 */
 	public String toString(String input) {
--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -13,60 +13,60 @@ import edu.gatech.oad.antlab.pkg2.*;
  * in the package structure
  */
  public class AntLabMain {
-
+    
     /**antlab11.java message class*/
     private AntLab11 ant11;
-
+    
     /**antlab12.java message class*/
     private AntLab12 ant12;
-
+    
     /**antlab21.java message class*/
     private AntLab21 ant21;
-
+    
     /**antlab22.java message class*/
     private AntLab22 ant22;
-
+    
     /**antlab31 java message class which is contained in a jar resource file*/
     private AntLab31 ant31;
-
-
-
+    
+    
+    
     /**
      * the constructor that intializes all the helper classes
      */
     public AntLabMain () {
-
+        
         ant11 = new AntLab11();
         ant12 = new AntLab12();
         ant21 = new AntLab21();
         ant22 = new AntLab22();
         ant31 = new AntLab31();
-
-
+        
+        
     }
-
+    
     /**
      * gathers a string from all the other classes and prints the message
-     * out to the console
-     *
+     * out to the console     
+     * 
      */
     public void printOutMessage() {
-
-        String toPrint =
+        
+        String toPrint = 
             ant11.getMessage() + ant12.getMessage() + ant21.getMessage()
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("Gabriel");
-		  toPrint += p1.toString("gwang340");
+		  Person1 p1 = new Person1("P1");
+		  toPrint += p1.toString("gburdell1");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("Wang");
-		  toPrint += p2.toString("gwang340-gtri");
+		  Person2 p2 = new Person2("P2");
+		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Meghavarnika Budati");
+		  toPrint += p3.toString("mbudati3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
@@ -75,23 +75,23 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
                   toPrint += p5.toString("gburdell5");
-
+		  
         System.out.println(toPrint);
-
-
+        
+        
     }
-
-
+     
+    
     /**
      * entry point for the program
      */
      public static void main(String[] args) {
-
+        
         new AntLabMain().printOutMessage();
-
-     }
-
-
-
-
- }
+        
+     } 
+    
+    
+    
+    
+ } 
